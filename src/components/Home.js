e43104770aba8318737f62061d7586e336a48caa
Colum John Lynagh <columjohnlynagh@gmail.com>
2019-07-18 13:13:46 +0100

    hardcoded additional users

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,9 @@ const Home = ({ setUser }) => {
           <option value="jessjelly">jessjelly</option>
           <option value="grumpy19">grumpy19</option>
           <option value="tickle122">tickle122</option>
+          <option value="cooljmessy">cooljmessy</option>
+          <option value="weegembump">weegembump</option>
+          <option value="happyamy2016">happyamy2016</option>
         </select>
 
         <img className="homepageimage" src="https://nerdist.com/wp-content/uploads/2019/03/jk-simmons-jjj-header-1200x676.jpg" alt="https://i.ytimg.com/vi/fY7kIbkmnpc/maxresdefault.jpg" />
