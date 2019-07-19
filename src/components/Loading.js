d793195cc9ffa9394a9b3e83aaa7a992c5174dce
Colum John Lynagh <columjohnlynagh@gmail.com>
2019-07-19 14:20:10 +0100

    refactored Loading component to a functional component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,12 @@
 import Spinner from 'react-spinner-material';
 import React from 'react';
 
-export default class Loading extends React.Component {
-  render() {
-    return (
-      <div>
-        <Spinner size={120} spinnerColor={'#333'} spinnerWidth={5} visible={true} />
-      </div>
-    );
-  }
-}
+const Loading = () => {
+  return (
+    <div>
+      <Spinner size={120} spinnerColor={'#333'} spinnerWidth={5} visible={true} />
+    </div>
+  );
+};
+
+export default Loading;
